da5bf9f0f7560d8dfd97df257196a0e7081a7849
RishabhRajSh <70883764+RishabhRajSh@users.noreply.github.com>
2022-08-03 18:24:35 +0530

    Time Implementation

--- gold-experience/src/index.js
+++ gold-experience/src/index.js
@@ -1,3 +1,5 @@
+
+// logic for weather card
 let weather = {
   apiKey: "9fccf9d133d749eb15f04824bde9e690",
   fetchWeather: function (city) {
@@ -7,10 +9,30 @@ let weather = {
       + this.apiKey
     )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => this.displayWeather(data));
 
   },
   displayWeather: function(data){
-
+    const {name} = data;
+    const{icon, description} =data.weather[0];
+    const {temp, humidity} = data.main;
+    const {speed} = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "C";
+    document.querySelector(".humidity").innerText = "Humidity :" + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed : " + speed + "km/h";
   }
 };
+
+
+// // logic for time display card
+
+let time = document.getElementById("current-Time");
+setInterval(()=>{
+let d = new Date();
+//  time.innerHTML = d.toLocaleTimeString();
+document.querySelector(".current-Time").innerText = d.toLocaleTimeString();
+},1000)
