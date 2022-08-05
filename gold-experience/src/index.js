ea3c8bce4d005386381321b849016ed674e86e16
RishabhRajSh <70883764+RishabhRajSh@users.noreply.github.com>
2022-08-05 17:40:49 +0530

    search implementation
    search implementation and content aware search implimented

--- gold-experience/src/index.js
+++ gold-experience/src/index.js
@@ -17,16 +17,31 @@ let weather = {
     const{icon, description} =data.weather[0];
     const {temp, humidity} = data.main;
     const {speed} = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
+    // console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
     document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "C";
     document.querySelector(".humidity").innerText = "Humidity :" + humidity + "%";
     document.querySelector(".wind").innerText = "Wind Speed : " + speed + "km/h";
-  }
-};
+    document.querySelector(".weather").classList.remove("Loading");
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name + "')"
 
+  },
+// search function
+search : function(){
+  this.fetchWeather(document.querySelector(".search-bar").value);
+}
+};
+document.querySelector(".search button").addEventListener("click",function(){
+  weather.search();
+  });
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+  if(event.key == "Enter"){
+  weather.search();
+  }
+});
 
 // // logic for time display card
 
@@ -36,3 +51,5 @@ let d = new Date();
 //  time.innerHTML = d.toLocaleTimeString();
 document.querySelector(".current-Time").innerText = d.toLocaleTimeString();
 },1000)
+
+weather.fetchWeather("Varanasi");
